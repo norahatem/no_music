eb3bc7ee12b31d56e7ffe4be00fdf2f9c1a1a87b
nora hatem <nurahasalem@gmail.com>
2024-08-04 16:31:41 +0100

    fixed the quality issue but now the video is with no audio :)

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from pytubefix import YouTube 
 
-# Initialize YouTube object with the video URL
-yt = YouTube("https://www.youtube.com/watch?v=McFCv5yiTUs")
+yt = YouTube("https://www.youtube.com/watch?v=H09PmP5tsy8")
 
-# Filter for mp4 streams with both video and audio
-mp4_streams = yt.streams.filter(file_extension='mp4', progressive=True)
+# mp4_streams = yt.streams.filter(file_extension='mp4', progressive=True)
+mp4_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_audio = True)
 
-# Check if any streams are found
+for stream in mp4_streams.order_by('resolution'):
+    print(stream)
+
 if mp4_streams:
-    # Get the video with the highest resolution
-    d_video = mp4_streams.get_highest_resolution()
+    # the get highest resolution function doesn't work for some reason so have to do it manually
+    # d_video = mp4_streams.get_highest_resolution()
+    # print("using the get highest resolution function", d_video)
+    d_video = max(mp4_streams, key=lambda s: int(s.resolution[:-1]) if s.resolution else 0)
+    # d_video = mp4_streams.order_by('resolution').last()
+    print("using the max function",d_video)
     
-    # Download the video
-    d_video.download(output_path="D:\Coding\mini_projects\musicRemover\original")
+    d_video.download(output_path="D:\Coding\mini_projects\musicRemover\original", filename = yt.title)
 else:
     print("No suitable streams found")
