49c2d831a910ef36b6eac773f3e5509edcb9bc67
nora hatem <nurahasalem@gmail.com>
2024-08-05 15:48:09 +0100

    created a class

--- main.py
+++ main.py
@@ -1,76 +1,20 @@
-from pytubefix import YouTube 
-import pytubefix
-import os
-import sys
+from vidDownloader import vidDownloader
 
-fname = ""
 
 def main():
-    global fname
+
     url, fname = get_url_and_name()
-    vid_streams, aud_stream , resolutions = vid_resolutions(url)
-    res = get_resolution(resolutions)
-    res = resolutions[res]
-    # print(vid_streams[res])
-    download_vid(vid_stream= vid_streams[res], audio_stream= aud_stream)
-    
+    downloader = vidDownloader(url=url, fname=fname)
+    downloader.available_resolutions()
+    downloader.get_res_choice()
+    downloader.download_vid()
+
 
 def get_url_and_name():
     url = input("Please enter the url of the video to be downloaded: ")
     name = input("Please enter the new filename of the video: ")
     return url, name if name else None
 
-def get_resolution(available_resolutions):
-    i=1
-    print("AVAILABLE RESOLUTIONS: ")
-    for res in available_resolutions:
-        print(f"{i}. {res}")
-        i+=1
-    
-    choice = int(input("Choose a resolution: "))
-    
-    if 1 <= choice < i :
-        return choice -1
-    else:
-        sys.exit("Invalid resolution")
 
-def vid_resolutions(url):
-    global fname
-    try:
-        yt = YouTube(url)
-    except pytubefix.exceptions.RegexMatchError:
-        sys.exit("url not valid")
-    if fname is None:
-        fname = yt.title
-    # print(f"Video: {yt.title}\nAvailable resolutions:")
-    vid_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_video = True).order_by("resolution")
-    aud_stream = yt.streams.filter(file_extension='mp4', adaptive=True, only_audio = True).order_by("abr").last()
-    resolutions = []
-    video_streams = {}
-
-    # store all available resolutions
-    for stream in vid_streams:
-        # print("before filtering", stream)
-        if stream.resolution not in video_streams:
-            video_streams[stream.resolution] = stream
-            resolutions.append(stream.resolution)
-            # print("after filtering", stream)
-    return video_streams, aud_stream, resolutions
-
-
-def download_vid(vid_stream, audio_stream):
-        
-    video_path = vid_stream.download(output_path="D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_vid.mp4")
-        
-    audio_path = audio_stream.download(output_path = "D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_aud.mp3")
-    output_path = f"D:/Coding/mini_projects/musicRemover/original/{fname}.mp4"
-    
-    # Use ffmpeg to merge and re-encode the video and audio streams
-    os.system(f'ffmpeg -i "{video_path}" -i "{audio_path}" "{output_path}"')
-
-    # remove mp4 and mp3 file after merging
-    os.remove(audio_path)
-    os.remove(video_path)
-    
 if __name__ == "__main__":
     main()
---
+++ vidDownloader.py
@@ -0,0 +1,91 @@
+from pytubefix import YouTube
+import pytubefix
+import os
+import sys
+import subprocess
+
+
+class vidDownloader:
+    def __init__(self, url, fname) -> None:
+        # initialise the file name and the url
+        self.fname = fname
+        self.url = url
+        # initialise the commaon conditions instead of writing them!
+        self.conditions = {"file_extension": "mp4", "adaptive": True}
+        # the path that the downloaded stuff will be stored in
+        self.path = "D:/Coding/mini_projects/musicRemover/original"
+        # initialising available resolutions and streams
+        self._resolutions = []
+        # this is the variable where we store the no-duplicate video streams, the other one vid_streams is for all of the,
+        self.video_streams = dict()
+
+    # getter method for resolutions
+    @property
+    def resolutions(self) -> None:
+        return self._resolutions
+
+    # function to store all available resolution plus the equivalent streams - no duplicates
+    def available_resolutions(self) -> None:
+        # try to open the url and exit if it is invalid
+        try:
+            self.yt = YouTube(self.url)
+        except pytubefix.exceptions.RegexMatchError:
+            sys.exit("url not valid")
+            
+        # check if the file name is empty, if so let be the title of the video
+        if self.fname is None:
+            self.fname = self.yt.title
+
+        # get all video and audio streams and store them in their respective para
+        # notice that this is because we use adaptive streams where video and audio are seperate
+        self.vid_streams = self.yt.streams.filter(
+            **self.conditions, only_video=True
+        ).order_by("resolution")
+        self.aud_stream = (
+            self.yt.streams.filter(**self.conditions, only_audio=True)
+            .order_by("abr")
+            .last()
+        )
+
+        # store all available resolutions - no duplicates
+        # store corresponding stream
+        for stream in self.vid_streams:
+            if stream.resolution not in self.video_streams:
+                self.video_streams[stream.resolution] = stream
+                self.resolutions.append(stream.resolution)
+
+    def get_res_choice(self) -> None:
+        i = 1
+        for res in self.resolutions:
+            print(f"{i}. {res}")
+            i += 1
+        self.res = int(input("Choose a resolution: "))
+
+        if 1 <= self.res < i:
+            self.res -= 1
+            # now self.res stores the actual resolution, eg: "144p", it is a str
+            self.res = self.resolutions[self.res]
+            # print(self.resolutions[self.res])
+        else:
+            sys.exit("Invalid resolution")
+
+    def download_vid(self) -> None:
+        
+        # this is the actual chosen video stream with the correct resolution
+        video_stream = self.video_streams[self.res]
+        
+        video_path = video_stream.download(
+            output_path=self.path, filename=f"{self.fname}_vid.mp4"
+        )
+        audio_path = self.aud_stream.download(
+            output_path=self.path, filename=f"{self.fname}_aud.mp3"
+        )
+        output_path = f"{self.path}/{self.fname}.mp4"
+
+        # Use ffmpeg to merge and re-encode the video and audio streams
+        subprocess.run(["ffmpeg", "-i", video_path, "-i", audio_path, output_path])
+        # os.system(f'ffmpeg -i "{video_path}" -i "{audio_path}" "{output_path}"')
+        
+        # remove mp4 and mp3 file after merging
+        os.remove(audio_path)
+        os.remove(video_path)
