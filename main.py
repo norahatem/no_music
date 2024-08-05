7ab313b0f7185b848b71af9b29cd93c748ddad94
nora hatem <nurahasalem@gmail.com>
2024-08-05 13:40:10 +0100

    now able to download videos based on user's resolution choice!

--- main.py
+++ main.py
@@ -3,62 +3,72 @@ import pytubefix
 import os
 import sys
 
+fname = ""
+
 def main():
+    global fname
     url, fname = get_url_and_name()
-    # download_vid(url, fname)
-    vid_resolutions(url)
+    vid_streams, aud_stream , resolutions = vid_resolutions(url)
+    res = get_resolution(resolutions)
+    res = resolutions[res]
+    # print(vid_streams[res])
+    download_vid(vid_stream= vid_streams[res], audio_stream= aud_stream)
     
 
 def get_url_and_name():
     url = input("Please enter the url of the video to be downloaded: ")
     name = input("Please enter the new filename of the video: ")
-    return url, name
+    return url, name if name else None
+
+def get_resolution(available_resolutions):
+    i=1
+    print("AVAILABLE RESOLUTIONS: ")
+    for res in available_resolutions:
+        print(f"{i}. {res}")
+        i+=1
+    
+    choice = int(input("Choose a resolution: "))
+    
+    if 1 <= choice < i :
+        return choice -1
+    else:
+        sys.exit("Invalid resolution")
 
 def vid_resolutions(url):
+    global fname
     try:
         yt = YouTube(url)
     except pytubefix.exceptions.RegexMatchError:
         sys.exit("url not valid")
-    print(f"Video: {yt.title}\nchoose one of the available resolutions:")
-    vid_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_video = True).order_by("resolution")
-    resolutions = []
-    # store all resolutions
-    for stream in vid_streams:
-        resolutions.append(stream.resolution)
-        
-    # this line is to remove duplicates as dicts cant have any duplicates
-    resolutions = list(dict.fromkeys(resolutions))
-    i = 1
-    for resolution in resolutions:
-        print(f"{i}. {resolution}")
-        i+=1
-        
-
-def download_vid(url, fname):
-    yt = YouTube(url)
-    # create seperate streams for audio and video
-    vid_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_video = True)
-    aud_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_audio = True)
-
     if fname is None:
-        fname = yt.title    
+        fname = yt.title
+    # print(f"Video: {yt.title}\nAvailable resolutions:")
+    vid_streams = yt.streams.filter(file_extension='mp4', adaptive=True, only_video = True).order_by("resolution")
+    aud_stream = yt.streams.filter(file_extension='mp4', adaptive=True, only_audio = True).order_by("abr").last()
+    resolutions = []
+    video_streams = {}
 
-    if vid_streams:
-        vid = vid_streams.order_by("resolution").last()
-        print("using the get highest resolution function", vid)
-        video_path = vid.download(output_path="D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_vid.mp4")
-        print(video_path)
+    # store all available resolutions
+    for stream in vid_streams:
+        # print("before filtering", stream)
+        if stream.resolution not in video_streams:
+            video_streams[stream.resolution] = stream
+            resolutions.append(stream.resolution)
+            # print("after filtering", stream)
+    return video_streams, aud_stream, resolutions
+
+
+def download_vid(vid_stream, audio_stream):
         
-    if aud_streams:
-        aud = aud_streams.order_by("abr").last()
-        print("using the highest abr rate", aud)
-        audio_path = aud.download(output_path = "D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_aud.mp3")
-        print(audio_path)
+    video_path = vid_stream.download(output_path="D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_vid.mp4")
         
+    audio_path = audio_stream.download(output_path = "D:\Coding\mini_projects\musicRemover\original", filename = f"{fname}_aud.mp3")
     output_path = f"D:/Coding/mini_projects/musicRemover/original/{fname}.mp4"
+    
     # Use ffmpeg to merge and re-encode the video and audio streams
-    os.system(f'ffmpeg -i {video_path} -i {audio_path} "{output_path}"')
+    os.system(f'ffmpeg -i "{video_path}" -i "{audio_path}" "{output_path}"')
 
+    # remove mp4 and mp3 file after merging
     os.remove(audio_path)
     os.remove(video_path)
     
